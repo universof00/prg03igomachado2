20834a070f0e3054b4bdf799a201c6f580a5db19
igo machado <igo@igo.com>
2025-11-08 22:08:57 -0300

    first commit

---
+++ src/main/java/br/com/ifba/curso/entity/Curso.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.curso.entity;
+
+/**
+ *
+ * @author igo
+ */
+public class Curso {
+    
+}
